83429728dc083aa3468bd3ec0f0f8c5cbfb1ecf2
r0ncl <47467902+r0ncl@users.noreply.github.com>
2019-03-25 20:50:05 -0400

    Update CompliantNode.py

--- Assignment_3/part_II/CompliantNode.py
+++ Assignment_3/part_II/CompliantNode.py
@@ -1,26 +1,33 @@
+from Node import Node
 
-#/* CompliantNode refers to a node that follows the rules (not malicious)*/
 class CompliantNode(Node):
-    
-    def __init__(self, p_graph, p_malicious, p_txDistribution, numRounds):
-         #       // IMPLEMENT THIS
-        pass
-    
+  self.followees = []
+  self.pendingTx = []
 
-    def setFollowees(self, followees): 
-#        // IMPLEMENT THIS
-        pass
+  def __init__(self, p_graph, p_malicious, p_txDistribution, numRounds):
+    self.p_graph = p_graph
+    self.p_malicious = p_malicious
+    self.p_txDistribution = p_txDistribution
+    self.numRounds = numRounds
 
-    def setPendingTransaction(self, pendingTransactions):
-#        // IMPLEMENT THIS
-      pass
-    
 
-    def sendToFollowers(self):
-#        // IMPLEMENT THIS
-       pass
+  def setFollowees(self, followees):
+    self.followees = followees
 
-    def receiveFromFollowees(self,candidates):
-#        // IMPLEMENT THIS
-        pass
-    
+  
+  def setPendingTx(self, pendingTx):
+    self.pendingTx = pendingTx
+
+  def sendToFollowers(self):
+    tx = self.pendingTx
+    self.pendingTx = []
+    return tx
+
+  def receiveFromFollowees(self, candidateList):
+    candidateFollowees = [i.sender for i in candidateList]
+    maliciousTrue = [ [i and i not in candidateFollowees] for i in self.followees]
+    self.pendingTx = [None if maliciousTrue[i.sender] else i.tx for i in candidateList]
+    self.pendingTx = list(set(self.pendingTx)).remove(None)
+
+
+  
