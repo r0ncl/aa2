f944efc94202bf35bf724367e4f83c4ea4fdcc3f
r0ncl <47467902+r0ncl@users.noreply.github.com>
2019-03-26 01:42:01 -0400

    Update CompliantNode.py

--- Assignment_3/part_II/CompliantNode.py
+++ Assignment_3/part_II/CompliantNode.py
@@ -15,18 +15,18 @@ class CompliantNode(Node):
     self.followees = followees
 
   
-  def setPendingTx(self, pendingTx):
+  def setPendingTransaction(self, pendingTx):
     self.pendingTx = pendingTx
 
   def sendToFollowers(self):
-    tx = self.pendingTx
-    self.pendingTx = []
-    return tx
+    #tx = self.pendingTx
+    #self.pendingTx = []
+    return self.pendingTx
 
   def receiveFromFollowees(self, candidateList):
     candidateFollowees = [i.sender for i in candidateList]
     pairs = zip(self.followees, range(len(self.followees)))
-    maliciousTrue = [i and k not in candidateFollowees for i,k in pairs]
+    maliciousTrue = [i and k not in candidateFollowees for i, k in pairs]
     self.pendingTx = [None if maliciousTrue[i.sender] else i.tx for i in candidateList]
     self.pendingTx = list(set(self.pendingTx)).remove(None)
 
